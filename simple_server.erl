44c692b8f8e0828bb9b9105715cb0899006d4969
Julien Blanchard <julien@sideburns.eu>
2013-05-20 11:58:17 +0200

    Initial commit

---
+++ simple_server.erl
@@ -0,0 +1,16 @@
+-module(simple_server).
+
+-export([start/0]).  %% public interface of server %%
+
+%% Send messages to server using name of its process "simple_server" e.g simple_server ! "message"
+start() ->
+    register(simple_server, spawn( fun() -> loop() end)).
+
+loop() ->
+    receive
+        "quit" ->  io:format("Quit command received. Closing down server");
+        "list" ->  io:format("List command received. Listing rooms"),
+                   loop();
+        Msg    ->  io:format("Received message: ~p~n", [Msg]),
+                   loop()		%% recursion keeps server alive and listening %%
+    end.
